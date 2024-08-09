43e69465b232a1344220e1b0b06273e680ed4af7
Otilia Nicola <otilianicola94@gmail.com>
2024-08-09 10:44:09 +0200

    Filter img sidebar and styles nav

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -13,43 +13,43 @@ export class DashboardComponent {
   days: Day[] = [
     {
       name: 'sun',
-      icon: 'https://static.vecteezy.com/system/resources/previews/019/899/727/original/simple-sun-icon-png.png',
+      icon: 'assets/images/sun.png',
       max: '44ºF',
       min: '37ºF'
     },
     {
       name: 'mon',
-      icon: 'https://static.vecteezy.com/system/resources/previews/019/899/727/original/simple-sun-icon-png.png',
+      icon: 'assets/images/sun.png',
       max: '39ºF',
       min: '33ºF'
     },
     {
       name: 'tue',
-      icon: 'https://static.vecteezy.com/system/resources/previews/019/899/727/original/simple-sun-icon-png.png',
+      icon: 'assets/images/sun.png',
       max: '39ºF',
       min: '32ºF'
     },
     {
       name: 'wed',
-      icon: 'https://static.vecteezy.com/system/resources/previews/019/899/727/original/simple-sun-icon-png.png',
+      icon: 'assets/images/sun.png',
       max: '37ºF',
       min: '31ºF'
     },
     {
       name: 'thu',
-      icon: 'https://cdn-icons-png.freepik.com/512/237/237867.png',
+      icon: 'assets/images/clouds.png',
       max: '40ºF',
       min: '32ºF'
     },
     {
       name: 'fri',
-      icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQb0rlws5JzmyEhMFu8CPqgDVHevM7qDQADWw&sg',
+      icon: 'assets/images/rain.png',
       max: '46ºF',
       min: '33ºF'
     },
     {
       name: 'sat',
-      icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQb0rlws5JzmyEhMFu8CPqgDVHevM7qDQADWw&sg',
+      icon: 'assets/images/rain.png',
       max: '58ºF',
       min: '51ºF'
     }
@@ -60,35 +60,35 @@ export class DashboardComponent {
     {
       name: 'Seattle',
       cod: 'US',
-      icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEG9OiHF1Q94pRZZt2yHVXAqWS501NzKjKag&s',
+      icon: 'assets/images/mist.png',
       temp: '37ºF',
       detail: 'Mist',
     },
     {
       name: 'Munich',
       cod: 'DE',
-      icon: 'https://cdn0.iconfinder.com/data/icons/weather-forecast-33/64/overcast_weather_climate_cloudy_sky-512.png',
+      icon: 'assets/images/cloud.png',
       temp: '23ºF',
       detail: 'Overcast Clouds',
     },
     {
       name: 'Paris',
       cod: 'FR',
-      icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEG9OiHF1Q94pRZZt2yHVXAqWS501NzKjKag&s',
+      icon: 'assets/images/mist.png',
       temp: '24ºF',
       detail: 'Mist',
     },
     {
       name: 'Istanbul',
       cod: 'TR',
-      icon: 'https://cdn-icons-png.flaticon.com/512/6697/6697662.png',
+      icon: 'assets/images/clear-sky.png',
       temp: '58ºF',
       detail: 'Clear Sky',
     },
     {
       name: 'Dubai',
       cod: 'AE',
-      icon: 'https://www.svgrepo.com/show/442419/weather-few-clouds-night-symbolic.svg',
+      icon: 'assets/images/few-clouds.png',
       temp: '71ºF',
       detail: 'Few Clouds',
     },
